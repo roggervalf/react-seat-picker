32e479614812131b25513e95d2ea6e306cf3ec86
Rogger794 <rogger.valverde@uni.pe>
2019-08-17 00:01:42 -0500

    adding loader and new function for accepting selections

--- example/src/App.js
+++ example/src/App.js
@@ -3,6 +3,21 @@ import React, { Component } from 'react'
 import SeatPicker from 'react-seat-picker'
 
 export default class App extends Component {
+  state = {
+    // token: uuid(),
+    loading:false
+  }
+  addSeatCallback=(row, number, id, cb)=>{
+    this.setState({
+      loading:true
+    },async()=>{
+      await new Promise(resolve => setTimeout(resolve, 5000));
+      console.log(`Added seat ${number}, row ${row}, id ${id}`)
+      if(row==="A")
+        cb(row, number, id)
+      this.setState({ loading: false })
+    })
+  }
   render () {
     const rows = [
       [{ number: 1, isSelected: true }, {number: 2}, null, {number: '3', isReserved: true, orientation: 'east'}, {number: '4', orientation: 'west'}, null, {number: 5}, {number: 6}],
@@ -11,10 +26,14 @@ export default class App extends Component {
       [{ number: 1 }, {number: 2}, null, {number: 3, orientation: 'east'}, {number: '4', orientation: 'west'}, null, {number: 5}, {number: 6}],
       [{ number: 1, isReserved: true }, {number: 2, orientation: 'east'}, null, {number: '3', isReserved: true}, {number: '4', orientation: 'west'}, null, {number: 5}, {number: 6, isReserved: true}]
     ]
+    const {loading}=this.state
+    console.log("loading", loading)
     return (
       <div>
         <h1>Seat Picker</h1>
-        <SeatPicker rows={rows} maxReservableSeats={3} alpha visible selectedByDefault />
+        <SeatPicker addSeatCallback={this.addSeatCallback}
+        rows={rows} maxReservableSeats={3} alpha visible selectedByDefault
+         />
       </div>
     )
   }
